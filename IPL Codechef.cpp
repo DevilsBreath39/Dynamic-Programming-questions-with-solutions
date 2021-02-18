7bec598fe9b3d0fcc5e350f7e468d7556c03753b
Rohit-Nandy39 <77715352+Rohit-Nandy39@users.noreply.github.com>
2021-02-18 22:33:10 +0530

    Create IPL Codechef.cpp

---
+++ IPL Codechef.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int maxProfit(vector<int>fees,int n)
+{
+    vector<int>dp(n);
+    dp[0]=fees[0];
+    dp[1]=fees[0]+fees[1];
+    dp[2]=max({fees[0]+fees[1],fees[0]+fees[2],fees[1]+fees[2]});
+
+    for(int i=3;i<n;i++)
+    {
+        dp[i]=max({fees[i]+fees[i-1]+dp[i-3],fees[i]+dp[i-2],dp[i-1]});
+    }
+
+    for(int i=0;i<n;i++)
+    {
+        cout<<dp[i]<<" ";
+    }
+    cout<<endl;
+    return dp[n-1];
+}
+
+
+int main()
+{
+    int n;
+    cout << "Enter the number of matches :- " << endl;
+    cin >> n;
+    vector<int> fees;
+    cout << "Enter the price of each match :- " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        fees.push_back(x);
+    }
+
+    int ans = maxProfit(fees, n);
+    cout << "Maximum profit :- " << ans << endl;
+}
