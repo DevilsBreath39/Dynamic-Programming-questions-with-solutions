f8f8f287285635fc2f6e88f38409d674aa605f6b
Rohit-Nandy39 <77715352+Rohit-Nandy39@users.noreply.github.com>
2021-02-18 22:37:48 +0530

    Create House Robber I.cpp

---
+++ House Robber I.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int maxProfit(vector<int> fees, int n)
+{
+    if (n==0)
+        return 0;
+
+    if (n==1)
+        return fees[0];
+
+
+    vector<int> dp(n);
+    dp[0]=fees[0];
+    dp[1]=max(fees[0],fees[1]);
+    
+
+    for(int i=2;i<n;i++)
+    {
+        dp[i]=max(fees[i]+dp[i-2],dp[i-1]);
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << dp[i] << " ";
+    }
+    cout << endl;
+    return dp[n-1];
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the number of houses :- " << endl;
+    cin >> n;
+    vector<int> fees;
+    cout << "Enter the loot at each house :- " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        fees.push_back(x);
+    }
+
+    int ans = maxProfit(fees, n);
+    cout << "Maximum profit :- " << ans << endl;
+}
